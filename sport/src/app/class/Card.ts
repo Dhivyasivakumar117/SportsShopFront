79f81d92bd8fa3537f88eb55527cb7a94195d3ff
Dhivyasivakumar117 <sudhivyavelan@gmail.com>
2021-07-01 10:28:49 +0530

    Final Front End

--- sport/src/app/class/Card.ts
+++ sport/src/app/class/Card.ts
@@ -2,6 +2,6 @@ export class Card {
     cardId : number = 0;
     cardNumber: number = 0;
     cardName: string = "";
-    cardExpiry: Date = new Date();
+    cardExpiry: Date | string = '' ;
     cvv: number = 0;
 }
